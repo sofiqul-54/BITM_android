d2604b34334af0565117451dee07715301c1d720
sofiqul-54 <sofiqul54@gmail.com>
2019-05-22 14:03:34 +0600

    bitm

---
+++ 03 Class/DemoApps/app/src/main/java/sofiqul54/com/demoapps/Image2Activity.java
@@ -0,0 +1,13 @@
+package sofiqul54.com.demoapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Image2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image2);
+    }
+}
