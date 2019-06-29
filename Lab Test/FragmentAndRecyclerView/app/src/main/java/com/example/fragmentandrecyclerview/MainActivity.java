482bd78c94bb711e6e29db11f27bfcd69d2f1d1c
sofiqul-54 <sofiqul54@gmail.com>
2019-06-29 15:38:38 +0600

    labtest

---
+++ Lab Test/FragmentAndRecyclerView/app/src/main/java/com/example/fragmentandrecyclerview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.fragmentandrecyclerview;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        replaceFragment(new HomeFragment());
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    replaceFragment(new HomeFragment());
+                    return true;
+                case R.id.navigation_cart:
+                    replaceFragment(new CartFragment());
+                    return true;
+                case R.id.navigation_profile:
+                    replaceFragment(new ProfileFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+}
---
+++ LabTestNew/ActivityRecylerView/app/src/main/java/com/sofiqul54/activityrecylerview/SeconndActivity.java
@@ -0,0 +1,15 @@
+package com.sofiqul54.activityrecylerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeconndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seconnd);
+
+    }
+}
