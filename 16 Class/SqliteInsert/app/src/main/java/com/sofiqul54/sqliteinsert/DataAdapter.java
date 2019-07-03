8eab9a2f5e572ded2c5add6bfa4ee0d5e93591f5
sofiqul-54 <sofiqul54@gmail.com>
2019-07-03 16:53:08 +0600

    insert sqlite

--- 16 Class/SqliteInsert/app/src/main/java/com/sofiqul54/sqliteinsert/DataAdapter.java
+++ 16 Class/SqliteInsert/app/src/main/java/com/sofiqul54/sqliteinsert/DataAdapter.java
@@ -3,9 +3,26 @@ package com.sofiqul54.sqliteinsert;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
-public class DataAdapter extends RecyclerView {
+public class DataAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     public DataAdapter(@NonNull Context context) {
         super(context);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
